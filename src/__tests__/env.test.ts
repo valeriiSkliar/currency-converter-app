aa784d78e54f8601d5c75eb0bf93d7eb3dce1e61
Valerii Skliarov <valeriisklyarov@gmail.com>
2026-05-24 17:35:16 +0300

    chore: remove eslint-disable comments for require imports and add jest type definitions

--- src/__tests__/env.test.ts
+++ src/__tests__/env.test.ts
@@ -16,25 +16,21 @@ describe("env", () => {
   });
 
   it("exposes EXPO_PUBLIC_API_URL", () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
     const Env = require("../../env").default;
     expect(Env.EXPO_PUBLIC_API_URL).toBe("http://localhost:8088");
   });
 
   it("exposes EXPO_PUBLIC_APP_SERVICE_KEY", () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
     const Env = require("../../env").default;
     expect(Env.EXPO_PUBLIC_APP_SERVICE_KEY).toBe("test-service-key");
   });
 
   it("has CurrencyConverter as app name", () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
     const Env = require("../../env").default;
     expect(Env.EXPO_PUBLIC_NAME).toBe("CurrencyConverter");
   });
 
   it("has cimmetria bundle IDs", () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
     const Env = require("../../env").default;
     expect(Env.EXPO_PUBLIC_BUNDLE_ID).toContain("cimmetria");
   });
